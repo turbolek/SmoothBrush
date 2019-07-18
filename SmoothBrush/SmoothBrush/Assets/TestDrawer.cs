6005203e404a3bce4450dc918b83f4947c7e4c80
Daniel Kunicki <dankunicki@gmail.com>
2019-07-19 00:11:23 +0200

    Print dots on catmull rom spline following mouse

--- SmoothBrush/SmoothBrush/Assets/TestDrawer.cs
+++ SmoothBrush/SmoothBrush/Assets/TestDrawer.cs
@@ -4,201 +4,115 @@ using UnityEngine;
 
 public class TestDrawer : MonoBehaviour
 {
-    List<Vector3> mousePositions;
+    private float velocity = 1f;
+    private List<Vector3> path = new List<Vector3>();
+
+    private Vector3 currentPosition;
+
     public GameObject brush1Prefab;
     public GameObject brush2Prefab;
     public GameObject brush3Prefab;
 
-    private Vector3 lastDrawnPoint;
+    [SerializeField]
+    private float brushSize = .25f;
+
+    Coroutine pathCoroutine = null;
+    Coroutine printingCoroutine = null;
 
     [SerializeField]
-    private float bufferZoneRadius;
-
-    [SerializeField]
-    private float epsilon = .5f;
-
-    private Vector3 anchor;
-
-    Coroutine samplingCoroutine = null;
-    Coroutine drawingCoroutine = null;
-
-    void Start()
-    {
-        mousePositions = new List<Vector3>();
-    }
-
+    private float pathRefreshTime = .1f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (samplingCoroutine != null)
+            if (pathCoroutine != null)
             {
-                StopCoroutine(samplingCoroutine);
+                StopCoroutine(pathCoroutine);
+            }
+            if (printingCoroutine != null)
+            {
+                StopCoroutine(printingCoroutine);
             }
 
-            samplingCoroutine = StartCoroutine(DrawingCoroutine());
+            pathCoroutine = StartCoroutine(PathCalculationCoroutine());
+            printingCoroutine = StartCoroutine(PrintCoroutine());
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            if (samplingCoroutine != null)
+            if (pathCoroutine != null)
             {
-                StopCoroutine(samplingCoroutine);
+                StopCoroutine(pathCoroutine);
             }
-            List<Vector3> simplePath = SimplifyPath(mousePositions);
-
-            foreach (Vector3 point in simplePath)
+            if (printingCoroutine != null)
             {
-                Instantiate(brush3Prefab, point, Quaternion.identity);
+                StopCoroutine(printingCoroutine);
             }
         }
 
-        //    if (mousePositions.Count >= 2 && drawingCoroutine == null)
-        //    {
-        //        drawingCoroutine = StartCoroutine(DrawSegment());
-        //    }
+        if (!Input.GetMouseButton(0))
+        {
+            transform.position = GetMouseWorldPosition();
+        }
     }
 
-    private IEnumerator DrawingCoroutine()
+    private IEnumerator PathCalculationCoroutine()
     {
-        mousePositions.Clear();
+        Vector3[] pathAnchors = new Vector3[3];
         while (true)
         {
-            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseWorldPosition.z = 0f;
-            mousePositions.Add(mouseWorldPosition);
-            Instantiate(brush2Prefab, mouseWorldPosition, Quaternion.identity);
+            path.Clear();
+            pathAnchors[0] = pathAnchors[1];
+            pathAnchors[1] = currentPosition;
+            pathAnchors[2] = GetMouseWorldPosition();
 
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-
-    private IEnumerator DrawSegment()
-    {
-        for (int i = 1; i <= 25; i++)
-        {
-            Vector3 positionToDraw = iTween.PointOnPath(GetDrawingPath(), (float)i / 25f);
-            Instantiate(brush3Prefab, positionToDraw, Quaternion.identity);
-            yield return null;
-        }
-        anchor = mousePositions[0];
-        mousePositions.RemoveAt(0);
-        drawingCoroutine = null;
-    }
-
-    Vector3[] GetDrawingPath()
-    {
-        Vector3[] path = new Vector3[2];
-        path[0] = mousePositions[0];
-        path[1] = mousePositions[1];
-        return path;
-    }
-
-    List<Vector3> SimplifyPath(List<Vector3> path)
-    {
-        if (path.Count <= 2)
-        {
-            return path;
-        }
-        else
-        {
-            float distance;
-            int index;
-            Vector3 furthestPoint = GetFurthestMiddlePointOfPath(path, out distance, out index);
-
-            if (distance <= epsilon)
+            for (float percent = 0.5f; percent <= 1f; percent += 0.01f)
             {
-                path.RemoveAt(index);
-                return SimplifyPath(path);
+                path.Add(iTween.PointOnPath(pathAnchors, percent));
             }
-            else
+            yield return new WaitForSeconds(pathRefreshTime);
+        }
+    }
+
+    private IEnumerator PrintCoroutine()
+    {
+        int currentIndex = 0;
+        List<Vector3> pointsToDraw = new List<Vector3>();
+        float distanceToDraw = 0f;
+
+        while (true)
+        {
+            distanceToDraw += velocity * Time.deltaTime;
+
+            while (distanceToDraw > 0f)
             {
-                List<Vector3> firstPart = SimplifyPath(path.GetRange(0, index + 1));
-                List<Vector3> secondPart = SimplifyPath(path.GetRange(index, path.Count - index));
-                secondPart.RemoveAt(0);
-                firstPart.AddRange(secondPart);
-                return firstPart;
-            }
-        }
-    }
-
-    Vector3 GetFurthestMiddlePointOfPath(List<Vector3> path, out float maxDistance, out int index)
-    {
-        Ray ray = new Ray(path[0], path[path.Count - 1] - path[0]);
-        maxDistance = 0f;
-        index = 0;
-        Vector3 furthestMiddlePoint = path[0];
-
-        for (int i = 1; i < path.Count - 1; i++)
-        {
-            Vector3 currentPoint = path[i];
-            float currentDistance = Vector3.Cross(ray.direction, currentPoint - ray.origin).magnitude;
-            if (currentDistance > maxDistance)
-            {
-                index = i;
-                maxDistance = currentDistance;
-                furthestMiddlePoint = currentPoint;
-            }
-        }
-        return furthestMiddlePoint;
-    }
-
-    public void DrawRawPositions()
-    {
-        foreach (Vector3 worldPosition in mousePositions)
-        {
-            Instantiate(brush1Prefab, worldPosition, Quaternion.identity);
-        }
-    }
-
-    public void DrawSmoothPositions()
-    {
-        Vector3[] smoothPositions = MakeSmoothCurve(mousePositions.ToArray(), 3f);
-        for (int i = 0; i < smoothPositions.Length; i++)
-        {
-            Vector3 pos = smoothPositions[i];
-            pos.z = -.1f;
-            Instantiate(brush3Prefab, pos, Quaternion.identity);
-        }
-        mousePositions.Clear();
-    }
-
-    public static Vector3[] MakeSmoothCurve(Vector3[] arrayToCurve, float smoothness)
-    {
-        List<Vector3> points;
-        List<Vector3> curvedPoints;
-        int pointsLength = 0;
-        int curvedLength = 0;
-
-        if (smoothness < 1.0f) smoothness = 1.0f;
-
-        pointsLength = arrayToCurve.Length;
-
-        curvedLength = (pointsLength * Mathf.RoundToInt(smoothness)) - 1;
-        curvedPoints = new List<Vector3>(curvedLength);
-
-        float t = 0.0f;
-        for (int pointInTimeOnCurve = 0; pointInTimeOnCurve < curvedLength + 1; pointInTimeOnCurve++)
-        {
-            t = Mathf.InverseLerp(0, curvedLength, pointInTimeOnCurve);
-
-            points = new List<Vector3>(arrayToCurve);
-
-            for (int j = pointsLength - 1; j > 0; j--)
-            {
-                for (int i = 0; i < j; i++)
+                if (path.Count > currentIndex + 1)
                 {
-                    points[i] = (1 - t) * points[i] + t * points[i + 1];
+                    pointsToDraw.Add(path[currentIndex]);
+                    distanceToDraw -= Vector3.Distance(path[currentIndex], path[currentIndex + 1]);
+                    currentIndex++;
+                }
+                else
+                {
+                    distanceToDraw = 0f;
                 }
             }
 
-            curvedPoints.Add(points[0]);
+            foreach (Vector3 point in pointsToDraw)
+            {
+                Instantiate(brush3Prefab, point, Quaternion.identity);
+                currentPosition = point;
+            }
+            yield return null;
         }
-
-        return (curvedPoints.ToArray());
     }
 
-
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorlPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorlPosition.z = 0f;
+        return mouseWorlPosition;
+    }
 }
