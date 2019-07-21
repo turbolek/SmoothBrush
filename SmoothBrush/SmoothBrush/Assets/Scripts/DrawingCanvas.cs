b3b6e6e53c68319b761c24268d2045e0ae540192
Daniel Kunicki <dankunicki@gmail.com>
2019-07-21 18:43:04 +0200

    Assign DrawingCanvas's target texture from code

--- SmoothBrush/SmoothBrush/Assets/Scripts/DrawingCanvas.cs
+++ SmoothBrush/SmoothBrush/Assets/Scripts/DrawingCanvas.cs
@@ -2,12 +2,17 @@
 using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent(typeof(RawImage))]
 public class DrawingCanvas : MonoBehaviour
 {
+    // Displays image from renderCamera on its own RawImage component
+
     [SerializeField]
     private Button clearButton;
     [SerializeField]
     private Camera renderCamera;
+
+    private RawImage rawImage;
 
     public void Init()
     {
@@ -19,10 +24,18 @@ public class DrawingCanvas : MonoBehaviour
             });
         }
 
+        rawImage = GetComponent<RawImage>();
+
+        if (renderCamera != null)
+        {
+            rawImage.texture = renderCamera.targetTexture;
+        }
 
     }
     private IEnumerator ClearCoroutine()
     {
+        //Flag camera to clear its render texture and disable clearing in next frame
+
         renderCamera.clearFlags = CameraClearFlags.SolidColor;
         yield return null;
         renderCamera.clearFlags = CameraClearFlags.Nothing;
